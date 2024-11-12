44a66e6d6d19e60f35f89484c5dfc78dd951b867
ncourtel <courtel.nattan19@gmail.com>
2024-11-12 14:38:10 +0100

    while and do while loop

--- basics/while-loops/while-loops.cpp
+++ basics/while-loops/while-loops.cpp
@@ -3,14 +3,27 @@
 using std::cout, std::getline, std::cin, std::string;
 
 int main() {
-
+   // While loop = basic while condition
    string name;
 
    while (name.empty()) {
       cout << "Enter your name: ";
       getline(cin, name);
    }
-   cout << "Hello " << name;
+   cout << "Hello " << name << '\n';
+
+
+   // do while loop = do some block of code first, 
+   //                 THEN repeat again if condition is true 
+
+   int number;
+
+   do { // Do the code first and check the condition after
+      cout << "Enter a positive number: ";
+      cin >> number;
+   } while(number < 0);
+   
+   cout << "The number is: " << number;
 
    return 0;
 }
