46a9f931cde5a59ed1b703722c95f27add52e9af
ncourtel <courtel.nattan19@gmail.com>
2024-11-12 14:43:50 +0100

    for loop

---
+++ basics/for-loop/for-loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using std::cout;
+
+int main() {
+   for (int i = 0; i < 10; i++){ // first variable, second condition, third iteration
+      cout << i << '\n';
+   }
+   return 0;
+}
