b6f3e83bfe11ddba897e7adb86d92b1416a80c3e
ncourtel <courtel.nattan19@gmail.com>
2024-11-14 16:59:41 +0100

    pointers introduction

--- basics/pointers/pointers.cpp
+++ basics/pointers/pointers.cpp
@@ -3,20 +3,23 @@
 int main() {
 
    std::string name = "Nollan";
-   std::string *pNmae = &name;
-   std::cout << *pNmae << '\n';
+   std::string *pName = &name; // Define a pointer 'pName' to store the address of 'name'
+   std::cout << *pName << '\n'; // Dereference 'pName' to access the value of 'name' and print it
 
    int age = 20;
-   int *pAge = &age;
-   std::cout << *pAge << '\n';
+   int *pAge = &age; // Define a pointer 'pAge' to store the address of 'age'
+   std::cout << *pAge << '\n'; // Dereference 'pAge' to access the value of 'age' and print it
+
    age = 25;
    std::cout << *pAge << '\n';
 
-   std::string grades[] = {"A", "B", "F"};
-   std::string *pGrades = grades;
-   std::cout << grades << '\n';
-   std::cout << pGrades << '\n';
-   std::cout << *pGrades << '\n';
+
+   char grades[] = {'A', 'B', 'F'};
+   char *pGrades = grades; // Define a pointer 'pGrades' that points to the first element of the 'grades' array
+
+   std::cout << grades << '\n'; // Print 'grades' as a C-style string, resulting in "ABF"
+   std::cout << pGrades << '\n'; // Print 'pGrades' as a C-style string, resulting in "ABF"
+   std::cout << *pGrades << '\n'; // Dereference 'pGrades' to print only the first character, 'A'
 
    return 0;
 }
